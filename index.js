e2165781d7950a7cb281ec7913e1db0d75c106b7
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-03-10 21:24:04 +0000

    Creating class for shotgun aircraft

--- index.js
+++ index.js
@@ -63,8 +63,31 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+
+        this.radius = 3
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'red'
+        c.fill()
+        c.closePath()
+    }
+
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
 
 const player = new Player()
+const projectiles = []
 const keys = {
     a: {
         pressed: false
@@ -83,6 +106,9 @@ function animated() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
+    projectiles.forEach(projectile => {
+        projectile.update()
+    })
 
     if (keys.a.pressed && player.position.x >= 0) {
         player.velocity.x = -7
@@ -110,6 +136,16 @@ addEventListener('keydown', ({key}) => {
             break
         case ' ':
             console.log('space')
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x + player.width / 2,
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -10
+                }
+            }))
             break
     }
 })

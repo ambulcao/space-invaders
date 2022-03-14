1e9b1c58cc19cdbce6d207364ed43a06b3a104b9
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-03-14 21:06:38 +0000

    Create class invader

--- index.js
+++ index.js
@@ -86,8 +86,49 @@ class Projectile {
     }
 }
 
+class Invader {
+    constructor() {
+            this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = './img/invader.png'
+        image.onload = () => {
+            const scale = 1
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height / 2
+            }
+        }
+    }
+
+    draw() {
+        c.drawImage(
+            this.image, 
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height
+        )
+    }
+    
+    update(){
+        if (this.image) {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        }
+    }
+}
+
 const player = new Player()
 const projectiles = []
+const invader = new Invader()
 const keys = {
     a: {
         pressed: false
@@ -105,6 +146,7 @@ function animated() {
     requestAnimationFrame(animated)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    invader.update()
     player.update()
     projectiles.forEach((projectile, index) => {
 

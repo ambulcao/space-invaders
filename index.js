1067008633f148b4be565fb23b7848bdec24b6df
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-03-18 22:18:13 +0000

    Shoot invaders

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ class Projectile {
         this.position = position
         this.velocity = velocity
 
-        this.radius = 3
+        //shooting radius
+        this.radius = 4
     }
 
     draw() {
@@ -211,8 +212,34 @@ function animated() {
 
     grids.forEach((grid) => {
         grid.update()
-        grid.invaders.forEach((invader) => {
+        grid.invaders.forEach((invader, i) => {
             invader.update({velocity: grid.velocity})
+            
+            //shooting enemies
+            projectiles.forEach((projectile,j) => {
+                if(projectile.position.y - projectile.radius <= 
+                        invader.position.y + invader.height && 
+                    projectile.position.x + projectile.radius >=
+                        invader.position.x && 
+                    projectile.position.x - projectile.radius <= 
+                        invader.position.x + invader.width && 
+                    projectile.position.y +  projectile.radius >= 
+                        invader.position.y
+                    ) {
+                        setTimeout(() => {
+                            const invaderFound = grid.invaders.find(
+                                (invader2) => invader2 === invader 
+                            )
+                            const projectileFound = projectiles.find(
+                                (projectile2) => projectile2 === projectile)
+
+                            if (invaderFound && projectileFound) {
+                                grid.invaders.splice(i, 1)
+                                projectiles.splice(j,1)
+                            }
+                        }, 0)
+                }
+            })
         })
     })
 
